1bd6a65dc5668d3452fd2e6b632ee3e7817f1d7d
Malcome Baggett <malcomebaggett@gmail.com>
2021-03-11 21:29:23 -0800

    added find visiblenodes

--- DFS_Tree/VisibleNodes.js
+++ DFS_Tree/VisibleNodes.js
@@ -1,14 +1,33 @@
 //In a binary tree, a node is considered "visible" if no node on the root-to-itself path has a greater value. The root is always "visible" 
 //since there are no other nodes between the root and itself. Given a binary tree, count the number of "visible" nodes.
-const bst = require('../BST/BST')
+const BST = require('../BST/BST')
 
 let bst = new BST.BST()
 
-bst.Insert(10)
+bst.Insert(5)
+bst.Insert(4)
 bst.Insert(6)
-bst.Insert(15)
 bst.Insert(3)
 bst.Insert(8)
-bst.Insert(20)
+    
 
 let bstRoot = bst.root
+
+const findVisibleNodes = (node) => {
+    return dfs(node, Number.NEGATIVE_INFINITY)
+}
+
+const dfs =( node, num) => {
+    if(!node) return 0
+    let total = 0
+
+    if (node.val >= num) {
+        total += 1
+    }
+
+    total += dfs(node.left, Math.max(num, node.val))
+    total += dfs(node.right, Math.max(num, node.right))
+    return total
+}
+
+console.log(findVisibleNodes(bstRoot))
