10618ce348b04047631247c94440357394fcf772
Malcome Baggett <malcomebaggett@gmail.com>
2021-04-03 09:36:51 -0400

    Added several delete methods

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -70,40 +70,105 @@ class LinkedList {
         }
 
         let curr = this.head
-        let prev = curr
 
-        while (curr) {
+     
+
             while (curr.next) {
                 if (curr.next.val === val) {
-                    deleted = curr.next.val
+                    deleted = curr.next
                     curr.next = curr.next.next
                 } else {
                     curr = curr.next
                 }
-           }
-        }
+
+            }
+ 
+
 
         if (this.tail.val === val) {
             deleted = this.tail
             this.tail = curr
 
         }
-
         return deleted
+    }
+
+    Reverse() {
+        if (!this.head) return null
+
+        let curr = this.head
+        let prev = null
+
+        while (curr) {
+            let temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        }
+
+        let temp = this.head
+        this.head = this.tail
+        this.tail = temp
+        return this
+    }
+
+    DeleteHead() {
+        if (!this.head) return null
+
+        let temp = this.head.next
+        let deleted = this.head
+        deleted.next = null
+        this.head = temp
+
+        return this
+        
+    }
+
+    DeleteTail() {
+        if (!this.head) return null
+
+        let curr = this.head
+        let prev = null
+
+        while (curr.next !== null) {
+            prev = curr
+            curr = curr.next
+        }
+
+        this.tail = prev
+        this.tail.next = null
+        
+        return curr
     }
 }
 
 let LL = new LinkedList()
 
-for(i = 0; i <= 15; i++) {
-    let num = Math.floor(Math.random() * Math.floor(15)) +1
-    LL.Append(num)
-}
+// for(i = 0; i <= 15; i++) {
+//     let num = Math.floor(Math.random() * Math.floor(15)) +1
+//     LL.Append(num)
+// }
 
+// console.log(LL.Print())
+// LL.Prepend(20)
+// console.log(LL.head)
+// console.log(LL.Print())
+// LL.Prepend(50)
+// console.log(LL.head)
+// console.log(LL.Print())
+
+LL.Append(1)
+LL.Append(2)
+LL.Append(5)
+LL.Append(7)
+LL.Append(15)
+
+// console.log(LL.Delete(2))
+// console.log(LL.Delete(15))
+// LL.Reverse()
+
+LL.DeleteHead()
+LL.DeleteTail()
 console.log(LL.Print())
-LL.Prepend(20)
 console.log(LL.head)
-console.log(LL.Print())
-LL.Prepend(50)
-console.log(LL.head)
-console.log(LL.Print())
+console.log(LL.tail)
