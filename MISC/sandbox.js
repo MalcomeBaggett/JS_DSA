73670b9ceeb845ed58cdf6a82fa369e8eb475b16
Malcome Baggett <malcomebaggett@gmail.com>
2021-04-21 22:47:54 -0400

    add map practice

---
+++ MISC/sandbox.js
@@ -0,0 +1,11 @@
+var list1 = [
+    { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+    { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
+  ]
+
+
+let list2 = list1.map((el) => {
+    return {...el, "new": 1}
+})
+
+console.log(list2)
