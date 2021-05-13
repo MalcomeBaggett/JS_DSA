6609a77626136ff565186a8c7131d0cfcc5db519
Malcome Baggett <malcomebaggett@gmail.com>
2021-05-13 07:54:49 -0400

    added move zeroes

---
+++ ArrayChallenges/moveZeroesTOENd.js
@@ -0,0 +1,22 @@
+const testArr = [1, 1, 0, 1, 0, 1, 1, 2, 0];
+
+const moveZero = (arr) => {
+  let count = 0;
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    console.log("hit");
+    if (arr[i] !== 0) {
+      arr[count] = arr[i];
+      count++;
+    }
+  }
+  console.log(arr);
+
+  for (let i = count; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+
+  return arr;
+};
+
+console.log(moveZero(testArr));
